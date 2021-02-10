cd0387fefb4b6fe82b52a4b424e12b042b185470
chomieu <chomieu@gmail.com>
2021-02-09 18:25:11 -0800

    Updated and commented server.js

--- server.js
+++ server.js
@@ -1,72 +1,28 @@
+// Dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+// Express app
 const PORT = process.env.PORT || 3000;
-
-const db = require("./models/Exercise.js");
-
 const app = express();
-
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
+// Handlebars
+app.engine("handlebars", exphbs({defaultLayout: "main"}))
+app.set("view engine", "handlebars")
+
+// Database
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
 
-db.Library.create({ name: "Campus Library" })
-  .then(dbLibrary => {
-    console.log(dbLibrary);
-  })
-  .catch(({message}) => {
-    console.log(message);
-  });
+// Routes
+const routes = require("./controller.js")
+app.use(routes)
 
-app.post("/submit", ({body}, res) => {
-  db.Book.create(body)
-    .then(({_id}) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
-    .then(dbLibrary => {
-      res.json(dbLibrary);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.get("/books", (req, res) => {
-  db.Book.find({})
-    .then(dbBook => {
-      res.json(dbBook);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.get("/library", (req, res) => {
-  db.Library.find({})
-    .then(dbLibrary => {
-      res.json(dbLibrary);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.get("/populated", (req, res) => {
-  db.Library.find({})
-    .populate("books")
-    .then(dbLibrary => {
-      res.json(dbLibrary);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
+// Server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
