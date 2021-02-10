12d863a8540bfd16220d563b2e54365b822df0ee
chomieu <chomieu@gmail.com>
2021-02-09 23:50:19 -0800

    Removed some mongoose configs

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ const mongoose = require("mongoose")
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
 })
 
 // Routes
