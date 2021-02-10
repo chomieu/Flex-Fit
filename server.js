3e09af18f82a600e4506c7c09b87f8cfcc145cd5
chomieu <chomieu@gmail.com>
2021-02-10 00:09:35 -0800

    Uninstalled morgan

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 // Express app
 const express = require("express")
-const logger = require("morgan")
 const PORT = process.env.PORT || 3000;
 const app = express()
-app.use(logger("dev"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static("public"))
